6fcaf96454d25cf2b39ea3d6d55787d033a09036
arielarreola <78185372+arielarreola@users.noreply.github.com>
2021-02-16 23:32:45 -0600

    Modificaciones hasta el martes 16/02/2021

--- server.js
+++ server.js
@@ -21,74 +21,18 @@ app.get('/',(req,res)=>{
 
 
 })
-app.get('/test',(req,res)=>{
-	//res.send("Hello test");
-	res.json({
-		prop1: "hello",
-prop2: "world"
-//json viewer extension
-
-})
-})
-
-//postroutes
-app.post('/post',(req,res)=>{
-console.log(req.body.prop1);
-aux=req.body.prop1
-//answer to cliente
-res.send(`Recibido: ${aux},${req.body.prop2}`);
-//estas comillas concatenan
-})
-//testing de api Axios
-//pueda consumir apis de internet
-app.get('/rick',(req,res)=>{
-const END_POINT='https://rickandmortyapi.com/api/character';
-
-axios.get(END_POINT)//traerme una respuesta
-	.then(function(response){
-		console.log(response.data)
-		res.send(response.data)
-
-})
-.catch(function(error){
-console.log(error);
-res.send(error);
-});
-})
-//fin axios
-
-
 //testing de api Axios
 //pueda consumir apis de internet
 //usando parametros
-app.get('/rick/:id',(req,res)=>{
-const END_POINT=`https://rickandmortyapi.com/api/character/${req.params.id}`;
-
-axios.get(END_POINT)//traerme una respuesta
-	.then(function(response){
-		console.log(response.data)
-//array=response.data
-		res.send(response.data)
-
-})
-.catch(function(error){
-console.log(error);
-res.send(error);
-});
-})
-//fin axios
-
 //insertadotest
 
 
-app.get('/randomrestroom',(req,res)=>{
-const END_POINT='https://www.refugerestrooms.org/api/v1/restrooms.json';
-
+app.get('/diasfestivos/:anio/:codpais',(req,res)=>{
+const END_POINT=`https://date.nager.at/api/v2/PublicHolidays/${req.params.anio}/${req.params.codpais}`;
 axios.get(END_POINT)//traerme una respuesta
 	.then(function(response){
 		console.log(response.data)
 		res.send(response.data)
-
 })
 .catch(function(error){
 console.log(error);
@@ -97,18 +41,13 @@ res.send(error);
 })
 
 
-
-//fin axios
-
-
+//fin diasfestivos
 
 app.get('/clima/:ciudad',(req,res)=>{
 const END_POINT=`http://api.openweathermap.org/data/2.5/weather?q=${req.params.ciudad}&units=metric&appid=f6902099c5861eb71633b6ef9828c452&lang=es`;
-
 axios.get(END_POINT)//traerme una respuesta
 	.then(function(response){
 		console.log(response.data)
-//array=response.data
 		res.send(response.data)
 
 })
@@ -211,7 +150,6 @@ axios.get(END_POINT)//traerme una respuesta
 		console.log(response.data)
 //array=response.data
 		res.send(response.data)
-
 })
 .catch(function(error){
 console.log(error);
@@ -219,72 +157,103 @@ res.send("error");
 });
 })
 
-app.post('/crearObjeto1',(req,res)=>{
+app.post('/crearObjeto',(req,res)=>{
 console.log(req.body);
-fetch('https://jsonplaceholder.typicode.com/posts', {
-  method: 'POST',
-  body: JSON.stringify({
-    title: `${req.body.title}`,
-    body: `${req.body.body}`,
-    userId: `${req.body.userId}`,
-  }),
-  headers: {
-    'Content-type': 'application/json; charset=UTF-8',
-  },
-})
-  .then((response) => response.json())
-  .then((json) => console.log(json));
-console.log(req.body);
-console.log(req.params);
+axios.post('https://jsonplaceholder.typicode.com/posts', {
+    data: {
+      userId: `${req.body.userId}`,
+      title: `${req.body.title}`,
+      body: `${req.body.body}`
+    }
+  }) 
+      .catch(function(error) {
+      console.log(error);
+res.send("error");
+    })
+
 res.send('Objeto enviado');
-})
+})//fin crear objeto
 
 app.post('/appfeelings',(req,res)=>{
-fetch('https://sentim-api.herokuapp.com/api/v1/', {
-  method: 'POST',
-  body: JSON.stringify({ "text": "That's not good at all" }),
-  headers: {
-    'Content-type': 'application/json; charset=UTF-8',
-  },
-})
-  .then((res) => res.json())
-  .then((json) => console.log(json));
 console.log(req.body);
-console.log(req.params);
-res.send('Objeeto enviado');
+axios.post('https://sentim-api.herokuapp.com/api/v1/', {
+text:`${req.body.text}`
 })
+.then(function(json){
+ console.log(json.data)
+console.log(json.data.sentences)
+res.send('Objeto enviado');
+}).catch(function(error) {
+      console.log(error);
+res.send("error");
+    })
+
+})//fin app feelings
 
 app.post('/animeporimagen',(req,res)=>{
 fetch("https://trace.moe/api/search", {
   method: "POST",
-  body: JSON.stringify({"url": "https://i.blogs.es/10b691/tumba-de-las-luciernagas-poster/1366_2000.jpg"  }),
+  body: JSON.stringify({ image: "https://i.blogs.es/10b691/tumba-de-las-luciernagas-poster/1366_2000.jpg" }),
   headers: { "Content-Type": "application/json" },
-})
-  .then((res) => res.json())
-  .then((result) =>console.log(result));
-res.send('Objeeto enviado');
+}).then((res) => res.json())
+  .then((result) => {
+    console.log(result);
+  });
 })
 
-app.post('/animeporimagen1/',(req,res)=>{
-const END_POINT=`https://trace.moe/api/search?url=https://i.blogs.es/10b691/tumba-de-las-luciernagas-poster/1366_2000.jpg`;
-axios.post(END_POINT)//traerme una respuesta
-	.then(function(response){
-		console.log(response.data)
-//array=response.data
-		res.send('Recibido')
 
-})
-.catch(function(error){
-console.log(error);
+
+app.post('/personarandom',(req,res)=>{
+axios.get(`https://randomuser.me/api/?password=${req.body.password}&results=${req.body.results}&gender=${req.body.gender}&nat=${req.body.nat}
+
+`).then(function(json){
+console.log(json.data.results)
+console.log(req.body);
+res.send(json.data.results);
+}).catch(function(error) {
+      console.log(error);
 res.send("error");
-});
-})
-//fin axios
+    })
+
+})//fin app personarandom
+
+app.post('/acortarURL',(req,res)=>{
+axios.post(`https://cleanuri.com/api/v1/shorten`,{
+url:`${req.body.url}`
+}).then(function(json){
+console.log(json.data)
+console.log(req.body);
+res.send('Objeto enviado');
+}).catch(function(error) {
+      console.log(error);
+res.send("error");
+    })
+
+})//fin acortar URL
+
+
+app.post('/imagenes',(req,res)=>{
+axios.post(`https://image-charts.com/chart`,{
+cht:`${req.body.cht}`,
+chs:`${req.body.chs}`,
+chxt:`${req.body.chxt}`,
+chd:`${req.body.chd}`
+}).then(function(json){
+console.log(json.data)
+console.log(req.body);
+res.send('Objeto enviado');
+}).catch(function(error) {
+      console.log(error);
+res.send("error");
+    })
+
+})//fin acortar URL
+
+
+//https://api.archivelab.org/v1/images
+https://image-charts.com/chart
 
 //base 64 imatch converter api
-
-
-//https://sentim-api.herokuapp.com/
 
 //insertado test
 
@@ -292,4 +261,4 @@ app.listen(3000,()=>{
 console.log("server running on port 3000");
 })
 
-//localhost:3000
+//localhost3000
